52a575b3a8418af9a7cce96ae844dc1d2f2bcb78
Immigma <154471954+Immigma@users.noreply.github.com>
2024-04-08 18:18:29 +0200

    Update pytorch.py - added class ContinuousDataLoader
    For loading continuous batches with regard to a dict value given by the key continuous_dict_key.

--- petastorm/pytorch.py
+++ petastorm/pytorch.py
@@ -256,6 +256,141 @@ class DataLoader(LoaderBase):
         self.reader.join()
 
 
+class ContinuousDataLoader(LoaderBase):
+    """
+    A data loader adaptor for ``torch.utils.data.DataLoader``.
+
+    This class iterates and returns items from the Reader in batches.
+
+    This loader can be used as an iterator and will terminate when the reader used in the construction of the class
+    runs out of samples.
+    """
+
+    def __init__(self, reader, continuous_dict_key, batch_size=1, collate_fn=decimal_friendly_collate,
+                 shuffling_queue_capacity=0):
+        """
+        Initializes a data loader object, with a default collate.
+
+        Number of epochs is defined by the configuration of the reader argument.
+
+        An optional shuffling queue is created if shuffling_queue_capacity is greater than 0. No samples will be
+        returned to a user by the ``DataLoader`` until the queue is full. After that, batches of `batch_size`
+        will be created by uniformly sampling the shuffling queue. Once no more samples are available from the data
+        reader, the shuffling queue is allowed to be consumed till no further samples are available.
+
+        Note that the last returned batch could have less then ``batch_size`` samples.
+
+        NOTE: ``make_batch_reader`` has it's own ``shuffle_row_groups`` argument. It randomizes order in
+        which parquet row-groups are loaded and has no effect on the order of rows within each row-group. To achieve
+        row-level shuffling you should set shuffling_queue_capacity to a non zero value.
+
+        :param reader: petastorm Reader instance
+        :param batch_size: the number of items to return per batch; factored into the len() of this reader
+        :param collate_fn: an optional callable to merge a list of samples to form a mini-batch.
+        :param shuffling_queue_capacity: Queue capacity is passed to the underlying :class:`tf.RandomShuffleQueue`
+          instance. If set to 0, no shuffling will be done.
+        """
+        super(DataLoader, self).__init__()
+        self.reader = reader
+        self.continuous_dict_key = continuous_dict_key
+        self.batch_size = batch_size
+        self.collate_fn = collate_fn
+
+        # _batch_acc accumulates samples for a single batch.
+        self._batch_acc = []
+        self.shuffling_queue_capacity = shuffling_queue_capacity
+        self._in_iter = None
+
+    def _iter_impl(self):
+        """
+        The Data Loader iterator stops the for-loop when reader runs out of samples.
+        """
+        # As we iterate over incoming samples, we are going to store them in `self._batch_acc`, until we have a batch of
+        # the requested batch_size ready.
+
+        keys = None
+        if self.shuffling_queue_capacity > 0:
+            # We can not know what is the reasonable number to use for the extra capacity, so we set a huge number
+            # and give up on the unbound growth protection mechanism.
+            min_after_dequeue = self.shuffling_queue_capacity - 1
+            self._shuffling_buffer = RandomShufflingBuffer(self.shuffling_queue_capacity,
+                                                           min_after_retrieve=min_after_dequeue,
+                                                           extra_capacity=100000000)
+        else:
+            self._shuffling_buffer = NoopShufflingBuffer()
+
+        for row in self.reader:
+            # Default collate does not work nicely on namedtuples and treat them as lists
+            # Using dict will result in the yielded structures being dicts as well
+            row_as_dict = row._asdict()
+
+            keys = row_as_dict.keys()
+
+            # Promote some types that are incompatible with pytorch to be pytorch friendly.
+            _sanitize_pytorch_types(row_as_dict)
+
+            # Add rows to shuffling buffer
+            if not self.reader.batched_output:
+                self._shuffling_buffer.add_many([row_as_dict])
+            else:
+                # Transposition:
+                #   row_as_dict:        {'a': [1,2,3], 'b':[4,5,6]}
+                #   row_group_as_tuple: [(1, 4), (2, 5), (3, 6)]
+                # The order within a tuple is defined by key order in 'keys'
+                row_group_as_tuple = list(zip(*(row_as_dict[k] for k in keys)))
+
+                # Adding data as 'row-by-row' into a shuffling buffer. This is a pretty
+                # slow implementation though. Probably can comeup with a faster way to shuffle,
+                # perhaps at the expense of a larger memory consumption...
+                self._shuffling_buffer.add_many(row_group_as_tuple)
+
+            # _yield_batches will emit as much batches as are allowed by the shuffling_buffer (RandomShufflingBuffer
+            # will avoid underflowing below a certain number of samples to guarantee some samples decorrelation)
+            for batch in self._yield_batches(keys):
+                yield batch
+
+        # Once reader can not read new rows, we might still have a bunch of rows waiting in the shuffling buffer.
+        # Telling shuffling buffer that we are finished allows to deplete the buffer completely, regardless its
+        # min_after_dequeue setting.
+        self._shuffling_buffer.finish()
+
+        for batch in self._yield_batches(keys):
+            yield batch
+
+        # Yield the last and partial batch
+        if self._batch_acc:
+            yield self.collate_fn(self._batch_acc)
+
+    def _yield_batches(self, keys):
+        last_continuous_value = None
+        while self._shuffling_buffer.can_retrieve():
+            post_shuffled_row = self._shuffling_buffer.retrieve()
+            if not isinstance(post_shuffled_row, dict):
+                # This is for the case of batched reads. Here we restore back the
+                # dictionary format of records
+                post_shuffled_row = dict(zip(keys, post_shuffled_row))
+
+            current_continuous_value = post_shuffled_row[continuous_dict_key]
+            
+            # Batch is ready? Collate and emmit
+            if (not last_continuous_value is None and
+                (last_continuous_value != current_continuous_value or
+                 len(self._batch_acc) == self.batch_size)):
+                     yield self.collate_fn(self._batch_acc)
+                     self._batch_acc = []
+
+            self._batch_acc.append(post_shuffled_row)
+            last_continuous_value = current_continuous_value
+
+    # Functions needed to treat data loader as a context manager
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.reader.stop()
+        self.reader.join()
+
+
 class BatchedDataLoader(LoaderBase):
     """
     Same as DataLoader except it uses torch-based shuffling buffers which enable batched buffering
